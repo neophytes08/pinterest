57ec7c1a434a87a3f84a4143d352dea884b5ebca
neophytes08 <allan.alzula@gmail.com>
2015-06-09 13:17:04 +0800

    pinterest clone

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  resources :pins do
+    member do
+      put "like", to: "pins#upvote"
+    end
+  end
+
+  get '/profile' => 'pins#profile', as: 'profile_pin'
+  get '/all/pins' => 'pins#fetch'
+  get '/getpin/pin/:id' => 'pins#getPin'
+  get '/deletepin/pin/:id' => 'pins#destroy'
+  root 'pins#index'
+end
